792d5ad136f299cbba479da653c2d94b6fc58cd3
chqier <mothnna.chqier@gmail.com>
2018-11-08 12:02:44 +0100

    Show Kurse name on startup

--- install-alt.ps1
+++ install-alt.ps1
@@ -1,11 +1,10 @@
-﻿#mainscript
+﻿
 # RunAsAdministrator
 
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 # region Include required files
 #
-$Global:restart=1
 $ScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 try {
     . ("$ScriptDirectory\functions.ps1")
@@ -14,44 +13,23 @@ try {
 catch {
     Write-Host "Error while loading supporting PowerShell Scripts" 
 } #end the include
-    
-    
+
 
 #Logfile variable
-
-
-
-
+$s=Remove-Item –path "c:\choco.log"
+$s=Remove-Item –path "\\nas00.gfu.net\disk\temp\mothnna\array.txt"
 $Logfile = "c:\Choco.log"
-
-if (Test-Path -Path $Logfile){Remove-Item –path $Logpath}
  
 #Get RAum Nummer
 
 $raumNr=GetraumNr
+echo $raumNr
 if (($RaumNr -lt 1) -or ($RaumNr -gt 18 )){
     echo "Raum Nummer ist nicht erkennbar" 
     $RaumNr = Read-Host "Bitte geben Sie der Raum Nummer type"
 } 
-#getscript name
-$files=Get-ChildItem "\\nas00.gfu.net\disk\temp\raum\$raumNr"
-foreach ( $file in $files) {
-  $extn = [IO.Path]::GetExtension($file)
-  if ($extn -eq ""){
-       $scriptname=$file.name
-  }
-}
-if( -not $Scriptname){ $Scriptname="NUR DEFAULT script"}
-
 #--------------------------
-
-
-
-Write-host "  Setup startet im Raum  $raumNr, $scriptname wird ausgeführt" -ForegroundColor Green
-
-
-
-
+ 
 
 #run defaultscript
 DefaultScript
@@ -77,21 +55,16 @@ if (($RaumNr -lt 1) -or ($RaumNr -gt 18 )) {
 
 $l=Test-Path -path "C:\choco.log"
 if ($l){$log=Get-Content "c:\choco.log"}else{$log="NOLOG"}
+    
 
-
-If ($log -eq "NOLOG") 
-{
-    if ($restart -eq 1)
-    {
+If ($log -eq "NOLOG") {
     Write-Host "system will restart Press CTRL+c to cancel"
     for($i=10;$i -gt 0; $i--){
     write-host "-> $i  " -ForegroundColor DarkRed -BackgroundColor yellow -NoNewline
     Start-Sleep -s 1
     }
-     Restart-computer -force
- }
-    
    
+    #Restart-computer -force
  }else{
     echo $log
     Sendemail $log
